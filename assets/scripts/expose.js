01e07a0f9739e02edb6c44625f639bd005dbbe10
yue-hua-x <yux070@ucsd.edu>
2024-05-01 14:14:06 -0700

    finished updateDropdown

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,10 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  document.getElementById("horn-select").addEventListener('change', updateDropdown);
+}
+
+function updateDropdown(event){
+  document.getElementsByTagName('img')[0].src="/assets/images/"+event.target.value+".svg";
+  document.getElementsByTagName('audio')[0].src="/assets/audio/"+event.target.value+".mp3";
 }
