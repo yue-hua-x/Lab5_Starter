3e512791a8d930ddf078676f7823d6446332489a
sierra392 <ssm24816@gmail.com>
2024-05-01 14:28:10 -0700

    added volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,3 +5,25 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+let vol = document.getElementById("volume");
+vol.addEventListener("change", function() {
+  let obj = document.getElementsByTagName('audio');
+  let images = document.querySelectorAll("img");
+  let val = vol.value;
+  console.log(val/100);
+  obj.volume = val/100;
+  if(val == 0){
+    images[1].src = "assets/icons/volume-level-0.svg";
+    images[1].alt = "Volume Level 0";
+  }else if( val < 33){
+    images[1].src = "assets/icons/volume-level-1.svg";
+    images[1].alt = "Volume Level 1";
+  } else if (val < 67){
+    images[1].src = "assets/icons/volume-level-2.svg";
+    images[1].alt = "Volume Level 1";
+  }else{
+    images[1].src = "assets/icons/volume-level-3.svg";
+    images[1].alt = "Volume Level 1";
+  }
+
+});
