9b3f83925fb548061020bf01156bff66db2e2844
yue-hua-x <yux070@ucsd.edu>
2024-05-06 22:36:59 -0700

    fixing unmergeable branch

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,7 +3,7 @@
 import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(10);
+  expect(1+2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
